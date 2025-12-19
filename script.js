f7048416bc1602ad3abe573588cf6e1a45c6d7fd
Arcade0405 <minhnhut2004@gmail.com>
2025-12-19 13:01:09 +0700

    test4

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-async function connectBackend() {
-  try {
-    // URL backend của bạn (Render cung cấp)
-    const response = await fetch("https://backend-node-render-kscc.onrender.com/");
-
-    // Chuyển dữ liệu nhận được sang JSON
-    const data = await response.json();
-
-    // Hiển thị nội dung lên alert
-    alert("Kết nối thành công! Thông điệp từ backend: " + data.message);
-  } catch (error) {
-    console.error("Lỗi khi kết nối backend:", error);
-    alert("Không thể kết nối tới backend!");
-  }
+function hello() {
+  alert("Hello from frontend!");
 }
